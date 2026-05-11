f3bf30f93e4a37d5bd2c2577019f2e472add52dd
Riya4546 <riyasuresh4444@gmail.com>
2026-05-11 20:23:30 +0530

    content is done

---
+++ content.js
@@ -0,0 +1,18 @@
+let currentSpeed = 1;
+
+function applySpeed() {
+    const video = document.querySelector("video");
+
+    if (video) {
+        video.playbackRate = currentSpeed;
+    }
+}
+
+setInterval(applySpeed, 1000);
+
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.speed) {
+        currentSpeed = request.speed;
+        applySpeed();
+    }
+});
